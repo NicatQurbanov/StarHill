4768827159d4f2307e07379a164f41d5cdb536cc
NicatQurbanov <nicatqur1307@gmail.com>
2025-12-05 21:16:30 +0400

    to be continued

---
+++ JS/data.js
@@ -0,0 +1,32 @@
+export servicesArr = [
+    {
+        url: "",
+        header: "SUSTAİNABİLİTY",
+        par: "",
+    },
+    {
+        url: "",
+        header: "PRIVATE CLIENT SERVICES",
+        par: "",
+    },
+    {
+        url: "",
+        header: "CORPORATE SERVICES",
+        par: "",
+    },
+    {
+        url: "",
+        header: "CAPACITY BUILDING",
+        par: "",
+    },
+    {
+        url: "",
+        header: "EDUCATION",
+        par: "",
+    },
+    {
+        url: "",
+        header: "BUSINESS DEVELOPMENT",
+        par: "",
+    },
+]
